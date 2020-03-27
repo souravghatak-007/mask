5ff257553ffce7ec796e19252eda7a564c9b264a
souravghatak-007 <sourav.influxiq@gmail.com>
2020-03-27 22:12:16 +0530

    add modal

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit ,ViewChild} from '@angular/core';
+import { Component, OnInit ,ViewChild,Inject} from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormGroupDirective } from '@angular/forms';
 import {ApiService} from './../../api.service';
 import { from } from 'rxjs';
 import { CookieService } from "ngx-cookie-service";
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -13,7 +18,7 @@ export class HomeComponent implements OnInit {
   @ViewChild(FormGroupDirective, {static: false}) formDirective: FormGroupDirective;
   public myForm: FormGroup;
 public selected:any;
-  constructor(public formbuilder: FormBuilder,public apiService:ApiService,public cookie:CookieService) { 
+  constructor(public dialog: MatDialog,public formbuilder: FormBuilder,public apiService:ApiService,public cookie:CookieService) { 
     // this.apiService.gettemptoken().subscribe((res: any) => {
     //   this.cookie.set('jwtToken', res.token);
     // });
@@ -34,7 +39,9 @@ public selected:any;
 
   ngOnInit() {
   }
-  submit(){}
+  submit(){
+    this.openTermsDialog();
+  }
 
   gotoTop() {
     window.scroll({ 
@@ -43,6 +50,33 @@ public selected:any;
       behavior: 'smooth' 
     });
     console.warn('test');
+  }
+
+  openTermsDialog() {            //demo for dialog 
+    const dialogRef = this.dialog.open(Success, {
+      width: '250px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+     
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: './Smodal.html',
+})
+export class Success {
+
+  constructor(
+    public dialogRef: MatDialogRef<Success>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
